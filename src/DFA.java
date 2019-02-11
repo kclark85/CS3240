df1cfa0be5c7201286dc89513a3d95f63bd5c60d
kclark85 <kclark85@msudenver.edu>
2019-02-11 12:29:02 -0700

    update

--- src/DFA.java
+++ src/DFA.java
@@ -1,7 +1,4 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DFA{
     Set<State> Q;
@@ -20,64 +17,68 @@ public class DFA{
             this.F = accept;
     }
 
-    public boolean accepts(String input){
+    public boolean accepts(String input) {
         boolean isValid = false;
-        if(!Z.contains(input)){
-            isValid = false;
-        }
-        else if(Z.contains(input)){
-            isValid = true;
+        for (char c : input.toCharArray()) {
+            if (!Z.contains(String.valueOf(c))) {
+                isValid = false;
+            } else if (Z.contains(String.valueOf(c))) {
+                isValid = true;
+            }
         }
         return isValid;
     }
-
     public Set<State> acceptStates(){
-       Set<State> retset = new HashSet<>();
-        for (State s: this.F) {
-            retset.add(s);
-        }
-        return retset;
+        return this.F;
     }
 
     public Set<String> alphabet(){
-        Set<String> retset = new HashSet<>();
-        for (String s: this.Z) {
-            retset.add(s);
-        }
-        return retset;
+        return this.Z;
     }
 
     public State initialState(){
         return this.q0;
     }
 
-//    public State nextState(State source, String input){
+    public State nextState(State source, String input){
+        if (!this.Q.contains(source) || !this.Z.contains(input)) {
+            throw new NoSuchElementException("State or input not found");
+        }
+        return T.get(source).get(input);
+    }
+
+    public Set<State> states(){
+        return this.Q;
+    }
+    public Map<State,Map<String,State>> transitionFunction(){
+        return this.T;
+    }
+
+//    public static void main(String[] args) {
+////        State q0 = new State("q0", "start");
+//        State q1 = new State("q1", "zero");
+//        State q2 = new State("q2", "one");
+//        Map<State,Map<String,State>> tortillas = new HashMap<>();
+//        Map<String,State> tinytort = new HashMap<>();
+//        Set<State> tacos = new HashSet<>();
+//        Set<String> tortas = new HashSet<>();
+//        State tamales = new State("chicken", "cheese");
+//        State tomatillos = new State("dill", "pickle");
+//        Set<State> tostadas = new HashSet<>();
+//        tortas.add("a");
+//        tortas.add("b");
+//        tortas.add("c");
+//        tostadas.add(tamales);
+//        tostadas.add(tomatillos);
+//        DFA test = new DFA(tacos, tortas, tortillas, tamales, tostadas);
+//        tinytort.put("a", q1);
+//        tinytort.put("b", q2);
+//        tortillas.put(test.q0, tinytort);
+//        System.out.println(test.accepts("c"));
+//        System.out.println(test.acceptStates());
+//        System.out.println(test.alphabet());
+//        System.out.println(test.initialState());
 //
 //    }
-
-    public static void main(String[] args) {
-        State q1 = new State("q1", "zero");
-        State q2 = new State("q2", "one");
-        Map<State,Map<String,State>> tortillas = new HashMap<>();
-        Map<String,State> tinytort = new HashMap<>();
-        Set<State> tacos = new HashSet<>();
-        Set<String> tortas = new HashSet<>();
-        State tamales = new State("chicken", "cheese");
-        State tomatillos = new State("dill", "pickle");
-        Set<State> tostadas = new HashSet<>();
-        tortas.add("a");
-        tortas.add("b");
-        tortas.add("c");
-        tostadas.add(tamales);
-        tostadas.add(tomatillos);
-        DFA test = new DFA(tacos, tortas, tortillas, tamales, tostadas);
-        tinytort.put("a", q1);
-        tinytort.put("b", q2);
-        tortillas.put(test.q0, tinytort);
-        System.out.println(test.accepts("c"));
-        System.out.println(test.acceptStates());
-        System.out.println(test.alphabet());
-
-    }
 
 }
