7836d98331d45ffec60f581a4e2f378ff322c04e
kclark85 <kclark85@msudenver.edu>
2019-02-12 08:44:55 -0700

    donezo brozo!

--- src/DFA.java
+++ src/DFA.java
@@ -18,17 +18,23 @@ public class DFA{
     }
 
     public boolean accepts(String input) {
+        for (State s: Q) {
+            s.reset();
+        }
         boolean isValid = false;
         if(F.contains(q0) && (input == null || input.length() ==0)){
             return true;
         }
         State currentState = q0;
+        q0.increment();
         for(int i = 0; i < input.length(); i++){
-            //this.nextState(currentState, input.substring(i,i+1)).increment();
             currentState = this.nextState(currentState, input.substring(i,i+1));
+            if(currentState !=null )
             currentState.increment();
 
+
         }
+
         if(this.F.contains(currentState)){
             isValid = true;
         }
